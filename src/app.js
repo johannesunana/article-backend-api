0281eadd9f06d6e051d3a9601553f445ba344e16
Johannes Andrei Unana <99703415+johannesunana@users.noreply.github.com>
2026-04-08 13:57:54 +0800

    feat(auth): add user registration model stub and move app entry to src
    - move `app.js` to `src/app.js`
    - import `/auth/register` to call a new `createUser` function from `src/user/user.model.js`
    - return only `email` and `username` from the register endpoint

--- src/app.js
+++ src/app.js
@@ -2,49 +2,43 @@ import express from 'express';
 import responseTime from 'response-time';
 import StatsD from 'node-statsd';
 
-const app = express();
-const stats = new StatsD()
+// import { user } from './user/user.model.js';
+import {createUser} from './user/user.model.js';
 
+const app = express();
+// const stats = new StatsD()
 
 app.use(express.json());
-
-// app.use(responseTime( (req, res, time) => {
-// }));
 
 const loggingMiddleware = (req, res, next) => {
   console.log(`${req.method} - ${req.url}`);
   next();
 }
 
-app.use(responseTime((req, res, time) => {
-  var stat = (req.method + req.url).toLowerCase()
-    .replace(/[:.]/g, '')
-    .replace(/\//g, '_')
-  stats.timing(stat, time)
-}));
-
 app.get('/', loggingMiddleware, (req, res) => {
-  res
-  .status(200)
-  .send({
+  console.log(user);
+  res.status(200).send({
     msg: "Home Page"
   });
 });
 
 /* Register endpoint */
 app.post('/auth/register', loggingMiddleware, (req, res) => {
-  console.log(req.body);
-  res
-    .status(201)
-    .send(req.body);  // Created
+  console.log(req.body, "\nBody before passing to createUser: \n");
+  
+  createUser(req.body);
+  console.log("\nAfter createUser: \n");
+
+  res.status(201).send({
+    email: req.body.email,
+    username: req.body.username
+  });  // Created
 });
 
 /* Login endpoint */
 app.post('/auth/login', loggingMiddleware, (req, res) => {
   console.log(req.body);
-  res
-    .status(200)
-    .send(req.body);  // OK
+  res.status(200).send(req.body);  // OK
 });
 
 app.listen(3000, () => {
---
+++ src/user/user.model.js
@@ -0,0 +1,3 @@
+export const createUser = (body) => {
+  console.log(body);
+};
